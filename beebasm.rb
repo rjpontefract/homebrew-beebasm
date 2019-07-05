b0061890f05bc1dfb2e4f848e1df0b327fd75b9d
Rick Pontefract <rjpontefract@gmail.com>
2019-07-05 17:16:36 +1200

    Changes to make the formula build and install correctly

--- beebasm.rb
+++ beebasm.rb
@@ -1,14 +1,14 @@
-class beebasm < Formula
+class Beebasm < Formula
   desc 'A portable 6502 assembler with BBC Micro style syntax'
   homepage 'https://github.com/stardot/beebasm'
   head 'https://github.com/stardot/beebasm.git'
   
   def install
-    system 'make'
+    system 'cd src && make code'
     bin.install 'beebasm'
   end
 
   test do
-    system 'false'
+    system 'cd src && make run'
   end
 end
